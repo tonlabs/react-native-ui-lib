6b145b333a087fbd6d2db9f3ce12135b477419f1
Ethan Sharabi <ethans@wix.com>
2020-02-05 09:26:40 +0200

    create a simple showcase for the soft tapping issue

--- demo/src/screens/PlaygroundScreen.js
+++ demo/src/screens/PlaygroundScreen.js
@@ -1,15 +1,15 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
-import {View, Text, Card} from 'react-native-ui-lib'; //eslint-disable-line
+import {Colors, View, Text, Card, Incubator} from 'react-native-ui-lib'; //eslint-disable-line
 
 export default class PlaygroundScreen extends Component {
   render() {
     return (
       <View center bg-dark80 flex>
-        <Card height={100} center padding-20>
-          <Text text50>Playground Screen</Text>
-        </Card>
+        <Incubator.TouchableOpacity br100 bg-purple30 padding-20 feedbackColor={'red'} activeOpacity={1}>
+          <Text>Button</Text>
+        </Incubator.TouchableOpacity>
       </View>
     );
   }
